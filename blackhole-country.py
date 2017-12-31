89f21440affe70ba871a377156590a0b12533e95
Antonio Dias <accdias@gmail.com>
2017-12-31 12:40:51 -0500

    Make use of string format and print function

--- blackhole-country.py
+++ blackhole-country.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import requests
 import netaddr
 
 # For blocking by country list
-cidr_by_country_url_mask = 'http://www.ipdeny.com/ipblocks/data/countries/%s.zone'
+cidr_by_country_url_mask = 'http://www.ipdeny.com/ipblocks/data/countries/{}.zone'
 
 blocked_countries = ['cn', 'kr']
-blocked_countries_prefixes = {}
+blocked_prefixes = {}
 
 for country in blocked_countries:
-    r = requests.get(cidr_by_country_url_mask % country)
+    r = requests.get(cidr_by_country_url_mask.format(country))
     if r.ok:
-        blocked_countries_prefixes[country] = netaddr.cidr_merge(r.text.split())
+        blocked_prefixes[country] = netaddr.cidr_merge(r.text.split())
+r.close()
 
-for country in blocked_countries_prefixes:
-    print country
-    print blocked_countries_prefixes[country]
+for country in blocked_prefixes:
+    print('{}: Blocked {} addresses'.format(country.upper(), len(blocked_prefixes[country])))
