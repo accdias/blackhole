0b0174997ca7fe59b7921f58f17154d7cf232f8f
Antonio Dias <accdias@gmail.com>
2017-12-31 12:41:51 -0500

    Profiling speed of different IP addresses handling modules

---
+++ profile.py
@@ -0,0 +1,25 @@
+import cProfile
+import netaddr,ipaddr,ipaddress
+
+f=open('ip.txt', 'rb')
+a=f.read().splitlines()
+f.close()
+
+a=map(unicode,a)
+
+x=map(netaddr.IPNetwork, a)
+y=map(ipaddr.IPNetwork, a)
+z=map(ipaddress.ip_network, a)
+
+print 'netaddr.cidr_merge(x)'
+cProfile.run('xx=netaddr.cidr_merge(x)')
+
+print 'ipaddr.collapse_address_list(y)'
+cProfile.run('yy=ipaddr.collapse_address_list(y)')
+
+print 'ipaddress.collapse_addresses(z)'
+cProfile.run('zz=ipaddress.collapse_addresses(z)')
+
+print 'len(xx) = ', len(set(xx))
+print 'len(yy) = ', len(set(yy))
+print 'len(zz) = ', len(set(zz))
